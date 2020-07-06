d189ab78e05779c2f2b3a796d56a7c81a59d6e17
github_limjunho <jewel7492@naver.com>
2020-07-06 14:06:17 +0900

    Makefile

---
+++ procfs/read proc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void main(void)
+{
+	char buf[100];
+	int fd = open("/proc/wireless", O_RDWR);
+	read(fd, buf, 100);
+	printf("%s",buf);
+}	
